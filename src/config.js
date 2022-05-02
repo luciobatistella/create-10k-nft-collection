1c339d7743442bce09dcce9689903209c6b96f1e
Lucio Batistella <lucio.batistella@gmail.com>
2022-05-02 17:57:07 -0300

    .

--- src/config.js
+++ src/config.js
@@ -33,7 +33,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://lettucehands.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
